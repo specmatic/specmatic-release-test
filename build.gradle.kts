0234a8504ea5d84aa3cfdc48ad4559372867c386
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2025-08-18 11:27:56 +0530

    chore: fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
-import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 import io.specmatic.gradle.extensions.RepoType
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 plugins {
     id("io.specmatic.gradle") version "0.12.0"
@@ -9,12 +9,12 @@ specmatic {
     kotlinVersion = "2.1.20"
     kotlinApiVersion = KotlinVersion.KOTLIN_2_1
     releasePublishTasks =
-            listOf(
-                    "dockerBuildxPublish",
-                    "publishAllPublicationsToSpecmaticPrivateRepository",
-                    "publishAllPublicationsToSpecmaticSnapshotsRepository",
-                    "publishAllPublicationsToSpecmaticReleasesRepository",
-            )
+        listOf(
+            "dockerBuildxPublish",
+            "publishAllPublicationsToSpecmaticPrivateRepository",
+            "publishAllPublicationsToSpecmaticSnapshotsRepository",
+            "publishAllPublicationsToSpecmaticReleasesRepository",
+        )
 
     publishTo("specmaticPrivate", "https://repo.specmatic.io/private", RepoType.PUBLISH_ALL)
     publishTo("specmaticSnapshots", "https://repo.specmatic.io/snapshots", RepoType.PUBLISH_OBFUSCATED_ONLY)
@@ -28,7 +28,58 @@ specmatic {
             pom {
                 name = "Specmatic Release Test"
                 description =
-                        "TBD"
+                    "TBD"
+                url = "https://specmatic.io"
+                licenses {
+                    license {
+                        name = "Specmatic EULA"
+                        url = "https://specmatic.io/eula/"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "specmaticBuilders"
+                        name = "Specmatic Builders"
+                        email = "info@specmatic.io"
+                    }
+                }
+            }
+        }
+    }
+
+    withCommercialLibrary(project(":list")) {
+        githubRelease()
+
+        publish {
+            pom {
+                name = "Specmatic Release Test"
+                description =
+                    "TBD"
+                url = "https://specmatic.io"
+                licenses {
+                    license {
+                        name = "Specmatic EULA"
+                        url = "https://specmatic.io/eula/"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "specmaticBuilders"
+                        name = "Specmatic Builders"
+                        email = "info@specmatic.io"
+                    }
+                }
+            }
+        }
+    }
+    withCommercialLibrary(project(":utilities")) {
+        githubRelease()
+
+        publish {
+            pom {
+                name = "Specmatic Release Test"
+                description =
+                    "TBD"
                 url = "https://specmatic.io"
                 licenses {
                     license {
