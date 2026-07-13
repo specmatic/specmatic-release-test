7bdba2ca27cbf904d6bfae8df1042dea5a9be7fc
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2026-07-13 12:49:06 +0530

    sort out promotion

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,10 +13,22 @@ specmatic {
         )
 
 
+    promotion {
+        targetMavenRepository(
+            name = "specmaticReleases",
+            url = "https://repo.specmatic.io/releases",
+            type = RepoType.PUBLISH_OBFUSCATED_ONLY
+        )
+        canonicalMavenRepository = uri("https://repo.specmatic.io/snapshots")
+        dockerImage("specmatic/release-test-snapshot", "specmatic/release-test")
+    }
+
     withCommercialApplicationLibrary(project(":app")) {
         mainClass = "org.example.app.AppKt"
         githubRelease()
-
+        dockerBuild {
+            imageName = "release-test-snapshot"
+        }
         publishTo("specmaticPrivate", "https://repo.specmatic.io/private", RepoType.PUBLISH_ALL)
         publishTo("specmaticSnapshots", "https://repo.specmatic.io/snapshots", RepoType.PUBLISH_OBFUSCATED_ONLY)
         publishTo("specmaticReleases", "https://repo.specmatic.io/releases", RepoType.PUBLISH_OBFUSCATED_ONLY)
