3964b1f58053f24d5c5bb16866a2d83650a49406
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2026-07-13 11:48:48 +0530

    fix build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,4 +11,5 @@ dependencies {
     implementation(project(":utilities"))
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.13.4")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
--- list/build.gradle.kts
+++ list/build.gradle.kts
@@ -8,4 +8,5 @@ repositories {
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.13.4")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
